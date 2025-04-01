c4be74ce3f6d8d7e6fe9a60cb316a78c7daac055
vgmil <vgmiltonisaac@gmail.com>
2025-03-31 23:41:44 -0500

    refactor(HomeScreen): Extraer los estilos a una funcion.
    - Agregar la prop dimensions a (MainList)

--- src/Modules/Home/Components/MainList.flatList.component.tsx
+++ src/Modules/Home/Components/MainList.flatList.component.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {  StyleSheet, Animated, Text, Dimensions } from 'react-native';
+import { StyleSheet, Animated, Text, Dimensions } from 'react-native';
 
 interface MainListProps {
   data: any[];
-  renderItem: ({ item, index }: { item: any, index:number }) => React.ReactElement;
+  renderItem: ({ item, index }: { item: any, index: number }) => React.ReactElement;
   keyExtractor?: (item: any, index: number) => string;
-  itemSize:number;
-} 
+  itemSize: number;
+  dimensions: {
+    width: number;
+    height: number;
+  };
+}
 
 // Fix the component declaration syntax
 export const MainList: React.FC<MainListProps> = React.memo(({
@@ -14,60 +18,60 @@ export const MainList: React.FC<MainListProps> = React.memo(({
   renderItem,
   keyExtractor = (item: any, index: number) => index.toString(),
   itemSize,
+  dimensions,
+
 }) => {
+  if (data.length === 0) {
+    return <Text>No data available</Text>;
+  }
+  const scrollX = React.useRef(new Animated.Value(0)).current;
 
-const {width} = Dimensions.get('window');
-if (data.length === 0) {
-  return <Text>No data available</Text>;
-}
-const scrollX = React.useRef(new Animated.Value(0)).current;
-  
-return (
+  return (
 
-      <Animated.FlatList
-        data={data}
-        keyExtractor={keyExtractor}
-        renderItem={({item, index}) => {
+    <Animated.FlatList
+      showsHorizontalScrollIndicator={false}
+      horizontal
+      contentContainerStyle={{ alignItems: 'center' }}
+      snapToInterval={itemSize}
+      decelerationRate={0}
+      bounces={false}
+      onScroll={Animated.event(
+        [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+        { useNativeDriver: true },
+      )}
+      scrollEventThrottle={16}
+      data={data}
+      keyExtractor={keyExtractor}
+      renderItem={({ item, index }) => {
 
-          const inputRange = [
-            (index - 1) * itemSize,
-            index * itemSize,
-            (index + 1) * itemSize, 
-          ]
-          const scale = scrollX.interpolate({
-            inputRange,
-            outputRange: [0.75, 1, 0.75],
-            extrapolate: 'extend',
-          });
+        const inputRange = [
+          (index - 1) * itemSize,
+          index * itemSize,
+          (index + 1) * itemSize,
+        ]
+        const scale = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.75, 1, 0.75],
+          extrapolate: 'extend',
+        });
 
-          const styles = StyleSheet.create({
-            container: {
-              marginLeft: index == 0 ? (width - itemSize)/2: 0,
-              marginRight: index == data.length - 1? (width - itemSize)/2: 0,
-            },
-          });
+        const styles = StyleSheet.create({
+          container: {
+            marginLeft: index == 0 ? (dimensions.width - itemSize) / 2 : 0,
+            marginRight: index == data.length - 1 ? (dimensions.height - itemSize) / 2 : 0,
+          },
+        });
 
-          return (
-            <Animated.View style={[{ transform: [{ scale }] }, styles.container]}>
-              {renderItem({item, index})}
-            </Animated.View>
-            );
-        }}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        contentContainerStyle={{alignItems: 'center'}}
-        snapToInterval={itemSize}
-        decelerationRate={0}
-        bounces={false}
-        onScroll={Animated.event(
-          [{nativeEvent: {contentOffset: {x: scrollX}}}],
-          {useNativeDriver: true},
-        )}
-        scrollEventThrottle={16}
-      />
+        return (
+          <Animated.View style={[{ transform: [{ scale }] }, styles.container]}>
+            {renderItem({ item, index })}
+          </Animated.View>
+        );
+      }}
+    />
 
 
-  
+
   );
 });
 
--- src/Modules/Home/Home.screen.tsx
+++ src/Modules/Home/Home.screen.tsx
@@ -20,10 +20,8 @@ interface MovieResponse {
 }
 
 export const HomeScreen = ({ navigation }: any) => {
-  const { theme } = useTheme();
   const { width, height } = Dimensions.get('window');
   const ITEM_SIZE = width*0.7;
-
   const [isLoading, setIsLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
 
@@ -47,62 +45,36 @@ export const HomeScreen = ({ navigation }: any) => {
     setIsLoading,
   );
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: theme?.colors.background,
-    },
-    scrollViewContent: {
-      alignItems: 'center',
-    },
-    headerContainer: {
-      flex: 1,
-      paddingRight: 16,
-      width: '100%',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    CategoryHeader: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      color: theme.colors.text,
-      marginLeft: 16,
-    },
-    moviesContainer: {
-      marginTop: 20,
-    },
-  });
+
   if (ErrorNowPlaying) {
     return <Text>Error: {ErrorNowPlaying.toString()}</Text>;
   } 
-  console.log('isLoading', isLoading);
   if (!isLoading && nowPlaying.length === 0) {
     return <Text>No movies available</Text>;
   }
   if (isLoading) {
-    console.log('isLoading', isLoading);
     return <Loader />;
   }else  {
   return (
+
       <ScrollView
-        style={styles.container}
+        style={styles().container}
         bounces={false}
-        contentContainerStyle={styles.scrollViewContent}
+        contentContainerStyle={styles().scrollViewContent}
       >
-        <View style={styles.headerContainer}>
+        <View style={styles().headerContainer}>
           <SearchInput onSearch={() => 'hola'} />
           <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
-            <MaterialIcons
+            <MaterialIcons style={styles().icon}
               name="settings"
-              size={24}
-              color={theme?.colors.primary}
             />
           </TouchableOpacity>
         </View>
+
         <View>
-          <Text style={styles.CategoryHeader}>Now Playing</Text>
+          <Text style={styles().CategoryHeader}>Now Playing</Text>
           <MainList
+            dimensions={{ width, height }}
             itemSize={ITEM_SIZE}
             data={nowPlaying} 
             keyExtractor={(_,index) => index.toString()}
@@ -116,9 +88,45 @@ export const HomeScreen = ({ navigation }: any) => {
               />
             }
           />
-          
         </View>
       </ScrollView>
     );
   }
   }
+
+  export const styles =() =>{
+    const { theme } = useTheme();
+    return(
+    StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      },
+      scrollViewContent: {
+        alignItems: 'center',
+      },
+      headerContainer: {
+        flex: 1,
+        paddingRight: 16,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      },
+      icon:{
+        color:theme.colors.primary,
+        fontSize: 24
+      },
+      CategoryHeader: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: theme.colors.text,
+        marginLeft: 16,
+      },
+      moviesContainer: {
+        marginTop: 20,
+      },
+    })
+  )
+  
+  } 
